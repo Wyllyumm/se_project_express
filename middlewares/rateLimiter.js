8d251bfcced3f8a599fb84d984784b1b8f9c0e1f
Wyllyumm <walmozard@hotmail.com>
2025-01-23 17:46:37 -0500

    rate limit testing

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -3,13 +3,10 @@ const { rateLimit } = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   limit: 10, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
-  message: "Too many requests frp, tjos IP, please try again later.",
+  message: "Too many requests from, this IP, please try again later.",
   statusCode: 429,
   standardHeaders: true, // add the `RateLimit-*` headers to the response
   legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
-  onLimitReached: function (rq, res, options) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-  },
 });
 
 module.exports = { limiter };
