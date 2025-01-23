916594c35743387e34c3d556aaedcaf6769dd417
Wyllyumm <walmozard@hotmail.com>
2025-01-23 18:10:29 -0500

    rate limit testing

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -4,14 +4,17 @@ const RateLimitError = require("../errors/rateLimitError");
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   limit: 200, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
-  message: "Too many requests from, this IP, please try again later.",
   statusCode: 429,
   standardHeaders: true, // add the `RateLimit-*` headers to the response
   legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
-  onLimitReached: (req, res, options, err) => {
-    if (err.code === 429) {
-      throw new RateLimitError("Too many requests, please try again later.");
-    }
+  onLimitReached: (err, req, res, next) => {
+    console.error(err);
+    const { statusCode = 429 } = err;
+    res
+      .status(statusCode)
+      .send({
+        message: "Too many requests from, this IP, please try again later.",
+      });
   },
 });
 
