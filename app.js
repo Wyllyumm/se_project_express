1db018ab7866ab7d35e66a33e06d67b19c9bd95d
Wyllyumm <walmozard@hotmail.com>
2025-01-23 17:43:39 -0500

    rate limit testing

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "authorization"],
 };
+
 const { PORT = 3001 } = process.env;
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db", {
--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -3,8 +3,13 @@ const { rateLimit } = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   limit: 10, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
+  message: "Too many requests frp, tjos IP, please try again later.",
+  statusCode: 429,
   standardHeaders: true, // add the `RateLimit-*` headers to the response
   legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
+  onLimitReached: function (rq, res, options) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  },
 });
 
 module.exports = { limiter };
