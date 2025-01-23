bcc13426d464d0643a45d77466ab8a497302d50f
Wyllyumm <walmozard@hotmail.com>
2025-01-23 17:53:00 -0500

    rate limit testing

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const { rateLimit } = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  limit: 150, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
+  limit: 200, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
   message: "Too many requests from, this IP, please try again later.",
   statusCode: 429,
   standardHeaders: true, // add the `RateLimit-*` headers to the response
