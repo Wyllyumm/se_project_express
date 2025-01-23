a536d5519748050d4c647592d3b167a6ef30a27c
Wyllyumm <walmozard@hotmail.com>
2025-01-23 18:42:10 -0500

    rate limit update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cors());
 app.use(limiter);
 app.use(requestLogger);
 
-/*app.get("/crash-test", () => {
+/* app.get("/crash-test", () => {
   setTimeout(() => {
     throw new Error("Server will crash now");
   }, 0);
