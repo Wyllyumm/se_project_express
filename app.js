2436ca61caf02c28680fb8a2b4ab595fbe888f23
Wyllyumm <walmozard@hotmail.com>
2025-01-23 13:24:01 -0500

    rate limit testing

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { rateLimit } = require("express-rate-limit");
 
 const { errors } = require("celebrate");
 require("dotenv").config();
@@ -9,7 +8,7 @@ require("dotenv").config();
 const mainRouter = require("./routes/index");
 const { errorHandler } = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
-/*const { limiter } = require("./middlewares/rateLimiter"); */
+const { limiter } = require("./middlewares/rateLimiter");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -23,12 +22,6 @@ mongoose
   })
   .catch((e) => console.error(e));
 
-const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  limit: 10, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
-  standardHeaders: true, // add the `RateLimit-*` headers to the response
-  legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
-});
 app.use(express.json());
 app.use(cors());
 app.use(requestLogger);
